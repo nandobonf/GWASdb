86bbae68b973e72c7d38eac28bac038506f858a8
nandobonf <nandobonf@gmail.com>
2018-02-02 12:56:12 +0100

    dummy

--- GWASdb.R
+++ GWASdb.R
@@ -2,24 +2,24 @@
 library(data.table)
 library(magrittr)
 library(dplyr)
-dat1 <- data.table(SNP = paste0("rs",1:1e6)
-                  , CHR = sample(1:22, 1e6, replace = T)
-                  , POS = sample(1:1e6, 1e6, replace = T)
-                  , BETA = rnorm(1e6, 0, sd = 1)
-                  , SE = abs(rnorm(1e6,0,1))) %>% .[,P:=2*pnorm(-abs(BETA/SE))] %>% .[P != 0]
-
-dat2 <- data.table(SNP = paste0("rs",1:1e6)
-                  , CHR = sample(1:22, 1e6, replace = T)
-                  , POS = sample(1:1e6, 1e6, replace = T)
-                  , BETA = rnorm(1e6, 0, sd = 1)
-                  , SE = abs(rnorm(1e6,0,1))) %>% .[,P:=2*pnorm(-abs(BETA/SE))] %>% .[P != 0]
-
-# create the database
-
-
-mydb<- src_sqlite("GWAS.res.sqlite", create = TRUE) # create =TRUE creates a new database
-copy_to(mydb, df = dat1, temporary = F, name = "GWAS.data1", overwrite = T) # uploading data
-copy_to(mydb, df = dat2, temporary = F, name = "GWAS.data2", overwrite = T) # uploading data
+# dat1 <- data.table(SNP = paste0("rs",1:1e6)
+#                   , CHR = sample(1:22, 1e6, replace = T)
+#                   , POS = sample(1:1e6, 1e6, replace = T)
+#                   , BETA = rnorm(1e6, 0, sd = 1)
+#                   , SE = abs(rnorm(1e6,0,1))) %>% .[,P:=2*pnorm(-abs(BETA/SE))] %>% .[P != 0]
+# 
+# dat2 <- data.table(SNP = paste0("rs",1:1e6)
+#                   , CHR = sample(1:22, 1e6, replace = T)
+#                   , POS = sample(1:1e6, 1e6, replace = T)
+#                   , BETA = rnorm(1e6, 0, sd = 1)
+#                   , SE = abs(rnorm(1e6,0,1))) %>% .[,P:=2*pnorm(-abs(BETA/SE))] %>% .[P != 0]
+# 
+# # create the database
+# 
+# 
+# mydb<- src_sqlite("GWAS.res.sqlite", create = TRUE) # create =TRUE creates a new database
+# copy_to(mydb, df = dat1, temporary = F, name = "GWAS.data1", overwrite = T) # uploading data
+# copy_to(mydb, df = dat2, temporary = F, name = "GWAS.data2", overwrite = T) # uploading data
 
 
 mydb <- src_sqlite("GWAS.res.sqlite", create = FALSE)
